9c71934fda4669139ce541ef8c5003aa61e89f5f
Zack Hafezi <zack@hafezigroup.com>
2022-12-17 14:43:03 -0800

    Added ModItems class and created registerItems  and registerModItems methods.

---
+++ src/main/java/net/zackhafezi/tutorialmod/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.zackhafezi.tutorialmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.zackhafezi.tutorialmod.TutorialMod;
+
+
+
+public class ModItems {
+
+    public static final Item ZACK_INGOT = registerItem("zack_ingot",
+        new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name),item);
+    }
+    public static void registerModItems(){
+        TutorialMod.LOGGER.info("Registering Mod Items for "+ TutorialMod.MOD_ID);
+
+    }
+}
